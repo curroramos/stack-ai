6d59c2509e606acfd7230e040314eacd1365fbc8
curroramos <curro.ramosperez@gmail.com>
2025-04-15 20:16:11 -0500

    added database persistency

--- app/core/db.py
+++ app/core/db.py
@@ -1,7 +1,12 @@
 import os
 import json
 from pathlib import Path
+from threading import RLock
+from typing import Dict, Optional
 from app.models.library_models import Library
+from app.utils.indexing.indexing_service import IndexingService
+from app.utils.indexing.index_type import IndexType
+from app.utils.indexing.factory import create_index_by_type
 
 PERSIST_PATH = Path("data/db.json")
 
@@ -26,7 +31,7 @@ class InMemoryDB:
         for lid, lib_data in raw.items():
             library = Library(**lib_data)
             self._libraries[lid] = library
-            strategy = create_index_by_type(IndexType.LINEAR)  # or store index_type in metadata
+            strategy = create_index_by_type(IndexType.LINEAR)
             indexing_service = IndexingService(strategy)
             indexing_service.rebuild_index(library.chunk_map)
             self._indexing_services[lid] = indexing_service
@@ -37,8 +42,11 @@ class InMemoryDB:
         if indexing_service:
             indexing_service.rebuild_index(library.chunk_map)
         self._save_to_disk()
+    
+    def get_indexing_service(self, library_id: str) -> Optional[IndexingService]:
+        with self._lock:
+            return self._indexing_services.get(library_id)
 
-    # Use _persist_and_rebuild instead of duplicating logic in `update_library`
     def add_library(self, library: Library, index_type: IndexType = IndexType.LINEAR):
         with self._lock:
             self._libraries[str(library.id)] = library
@@ -47,6 +55,14 @@ class InMemoryDB:
             indexing_service.rebuild_index(library.chunk_map)
             self._indexing_services[str(library.id)] = indexing_service
             self._save_to_disk()
+
+    def get_library(self, library_id: str) -> Optional[Library]:
+        with self._lock:
+            return self._libraries.get(library_id)
+        
+    def list_libraries(self):
+        with self._lock:
+            return list(self._libraries.values())
 
     def update_library(self, library: Library):
         with self._lock:
@@ -57,3 +73,5 @@ class InMemoryDB:
             self._libraries.pop(library_id, None)
             self._indexing_services.pop(library_id, None)
             self._save_to_disk()
+
+db = InMemoryDB()
